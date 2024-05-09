1845ee037cc5ad0012864e24bedc4ca45526f3ce
Martin Rouault <martin.rlt24@gmail.com>
2024-05-09 18:24:20 +0200

    added image domains config in the next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['martin-rouault.vercel.app', 'localhost'],
+    },
+};
 
 export default nextConfig;
