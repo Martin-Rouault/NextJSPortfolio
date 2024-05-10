d79e9ef4c137f958dabdc99c653540da65833b76
Martin Rouault <martin.rlt24@gmail.com>
2024-05-11 00:25:46 +0200

    removed Image/nextjs import to use a default img tag

--- app/ui/pictureBackground/pictureBackground.tsx
+++ app/ui/pictureBackground/pictureBackground.tsx
@@ -1,11 +1,7 @@
-'use client';
-
-import Image from "next/image";
-
 export default function PictureBackground({ imageSrc }: { imageSrc: string }) {
   return (
     <div className="flex justify-center items-center mt-3 bg-neutral-800 rounded-md border-neutral-700 border-2">
-      <Image
+      <img
         src={imageSrc}
         width={1000}
         height={1000}
