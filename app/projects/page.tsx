c911eb625ec2debe8424e3adb66476f27a83b4a7
Martin Rouault <martin.rlt24@gmail.com>
2024-07-15 18:32:57 +0200

    remove unused code

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,3 @@
-import LatestProjects from "@/components/component/projects/projectLates";
-import ProjectList from "@/components/component/projects/projectList";
 import Header from "@/components/component/projects/header";
 import data from "@/app/locales/data.json";
 import { ProjectCard } from "../ui/block/projectCard";
