e7dcda48c415945cd38f97759452951a764ebaf2
Martin-Rouault <martin.rlt24@gmail.com>
2024-09-04 12:09:58 +0200

    test gtm link

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,13 @@ export default function Home() {
         <About />
         <LatestProjects headingLevel="h4" title="Latest Project" />
         <Contact />
-        <button
+        <a
+        href="https://www.google.com"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() =>
             sendGTMEvent({event: "buttonClicked", value: "xyz"})
           }
-        >Send Event</button>
+        >Send Event</a>
       </main>
       <NavBar />
     </>
