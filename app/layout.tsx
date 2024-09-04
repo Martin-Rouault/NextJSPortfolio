8d551cd42551db4943525ba6b81637465614a70a
Martin-Rouault <martin.rlt24@gmail.com>
2024-09-04 11:41:38 +0200

    test gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { GoogleTagManager } from "@next/third-parties/google"
+import { GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GeistSans } from "geist/font/sans";
@@ -24,9 +24,9 @@ export default function RootLayout({
       className={`${GeistSans.variable} ${GeistMono.variable}`}
       suppressHydrationWarning
     >
-      <GoogleTagManager gtmId="GTM-TR8CKLHN"></GoogleTagManager>
       <body className="antialiased max-w-xl mb-32 flex flex-col md:flex-row mx-4 mt-20 lg:mx-auto">
         <Providers>
+          <GoogleTagManager gtmId="GTM-TR8CKLHN" />
           <div className="pointer-events-none fixed left-0 top-0 z-50 h-12 w-full bg-neutral-100 to-transparent backdrop-blur-xl [-webkit-mask-image:linear-gradient(to_bottom,black,transparent)] dark:bg-neutral-900"></div>
           <main>{children}</main>
         </Providers>
