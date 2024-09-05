9dbf6f651bf96a03e18d9e257f449a896220257a
Martin-Rouault <martin.rlt24@gmail.com>
2024-09-05 09:49:48 +0200

    added _blank target to open in a new tab

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
         <Contact />
         <a
           href="https://app.captainvegetal.fr/"
+          target="_blank"
           className="gtm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => sendGTMEvent({ event: "buttonClicked", value: "xyz" })}
         >
